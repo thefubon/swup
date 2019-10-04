2c759fb2b9978e54e63c15815301c79773087baa
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2019-10-04 16:29:21 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+/* Данное решение пока не получилось реализовать */
+
+$('.uk-offcanvas-close').on('click', function () {
+    UIkit.offcanvas('#offcanvas-usage').toggle();
+});
+
+$('.uk-nav').on('click', function () {
+    UIkit.offcanvas('#offcanvas-usage').toggle();
+});
+
+$('.uk-nav').on('click', function () {
+    UIkit.offcanvas('#offcanvas-usage').toggle();
+});
+
+$('.uk-parent').on('click', function () {
+    UIkit.offcanvas('#offcanvas-usage').toggle();
+});
+
+$('a').on('click', function () {
+    UIkit.offcanvas('#offcanvas-usage').toggle();
+});
+
+$('.mc').on('click', function () {
+  UIkit.offcanvas('#offcanvas-usage').toggle();
+});
+
+$('.uk-nav > li > a').click(function() {
+   UIkit.offcanvas('#offcanvas-usage').toggle();
+});
